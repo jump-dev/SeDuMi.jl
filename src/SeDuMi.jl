239d181085ffada649dd1213601fe0a6a34c7e27
Benoît Legat <benoit.legat@gmail.com>
2019-01-01 13:52:11 +0100

    ✨ Add options

--- src/SeDuMi.jl
+++ src/SeDuMi.jl
@@ -1,12 +1,29 @@
 module SeDuMi
 
+using SparseArrays
 using MATLAB
 
 export sedumi
 
-# Solve the LP in standard form: min c'x, s.t. Ax = b, x ≥ 0
-function sedumi(A::Matrix{Float64}, b::Vector{Float64}, c::Vector{Float64})
-    return mxcall(:sedumi, 3, A, b, c)
+struct Cone
+    f::Float64 # number of free primal variables / linear dual equality constraints
+    l::Float64 # length of LP cone
+    q::Vector{Float64} # list of second-order cone dimension
+    r::Vector{Float64} # list of rotated second-order cone dimension
+    s::Vector{Float64} # list of semidefinite constraints
+end
+
+# Solve the primal/dual pair
+# min c'x,      max b'y
+# s.t. Ax = b,   c - Ax ∈ K
+#       x ∈ K
+function sedumi(A::Union{Matrix{Float64}, SparseMatrixCSC{Float64}},
+                b::Vector{Float64}, c::Vector{Float64},
+                K::Cone = Cone(0, size(A, 2), Float64[], Float64[], Float64[]);
+                kws...)
+    pars = Dict{String, Any}(string(key) => value for (key, value) in kws)
+    # There are 3 output arguments: x, y and info so we use `3` above
+    return mxcall(:sedumi, 3, A, b, c, K, pars)
 end
 
 end # module
--- test/lp.jl
+++ test/lp.jl
@@ -7,7 +7,7 @@ using SeDuMi
     A = [10.0 -7.0 -1.0 0.0
           1.0  0.5  0.0 1.0]
     b = [5.0, 3.0]
-    sol, dual, status = sedumi(A, b, c)
+    sol, dual, status = sedumi(A, b, c, fid=0)
     @test sol ≈ sparse([1, 2], [1, 1], [47/24, 25//12], 4, 1)
     @test dual ≈ [1/8, -1/4]
     @test iszero(status["pinf"])
