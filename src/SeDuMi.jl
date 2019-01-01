25aa219d6099dee914e7b53cb0bea4a23a326d02
Benoît Legat <benoit.legat@gmail.com>
2019-01-01 11:44:36 +0100

    Initial commit

---
+++ src/SeDuMi.jl
@@ -0,0 +1,10 @@
+module SeDuMi
+
+export sedumi
+
+# Solve the LP in standard form: min c'x, s.t. Ax = b, x ≥ 0
+function sedumi(A::Matrix{Float64}, b::Vector{Float64}, c::Vector{Float64})
+    return mxcall(:sedumi, 3, A, b, c)
+end
+
+end # module
