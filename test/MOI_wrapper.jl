2c6a06b65c5437da913ff97f1b5edcc86de34d4c
Benoît Legat <benoit.legat@gmail.com>
2021-10-27 09:55:31 -0700

    MOI.DeprecatedTest -> MOI.Test

--- test/MOI_wrapper.jl
+++ test/MOI_wrapper.jl
@@ -1,49 +1,78 @@
+module TestSeDuMi
+
 using Test
-
 using MathOptInterface
-const MOI = MathOptInterface
-const MOIT = MOI.DeprecatedTest
-const MOIU = MOI.Utilities
-const MOIB = MOI.Bridges
-
 import SeDuMi
-const OPTIMIZER_CONSTRUCTOR = MOI.OptimizerWithAttributes(SeDuMi.Optimizer, MOI.Silent() => true)
-const OPTIMIZER = MOI.instantiate(OPTIMIZER_CONSTRUCTOR)
 
-@testset "SolverName" begin
-    @test MOI.get(OPTIMIZER, MOI.SolverName()) == "SeDuMi"
+const MOI = MathOptInterface
+
+function runtests()
+    for name in names(@__MODULE__; all = true)
+        if startswith("$(name)", "test_")
+            @testset "$(name)" begin
+                getfield(@__MODULE__, name)()
+            end
+        end
+    end
+    return
 end
 
-const BRIDGED = MOI.instantiate(OPTIMIZER_CONSTRUCTOR, with_bridge_type=Float64)
-const CONFIG = MOIT.Config(atol=1e-4, rtol=1e-4)
-
-@testset "Unit" begin
-    MOIT.unittest(BRIDGED, CONFIG, [
-        # `NumberOfThreads` not supported.
-        "number_threads",
-        # `TimeLimitSec` not supported.
-        "time_limit_sec",
-        # Error using pretransfo (line 149)
-        # Size b mismatch
-        "solve_unbounded_model",
-        # Need https://github.com/jump-dev/MathOptInterface.jl/issues/529
-        "solve_qp_edge_cases",
-        # Integer and ZeroOne sets are not supported
-        "solve_integer_edge_cases", "solve_objbound_edge_cases",
-        "solve_zero_one_with_bounds_1",
-        "solve_zero_one_with_bounds_2",
-        "solve_zero_one_with_bounds_3"])
+function test_solver_name()
+    @test MOI.get(SeDuMi.Optimizer(), MOI.SolverName()) == "SeDuMi"
 end
 
-@testset "Continuous linear problems" begin
-    MOIT.contlineartest(BRIDGED, CONFIG, ["linear13"]) # See https://github.com/blegat/SeDuMi.jl/issues/7
+function test_runtests()
+    model = MOI.instantiate(SeDuMi.Optimizer, with_bridge_type=Float64)
+    MOI.set(model, MOI.Silent(), true)
+    MOI.Test.runtests(
+        model,
+        MOI.Test.Config(
+            rtol = 1e-4,
+            atol = 1e-4,
+            exclude = Any[
+                MOI.ConstraintFunction, # The bridge does not implement this
+                MOI.ConstraintBasisStatus,
+                MOI.VariableBasisStatus,
+                MOI.ConstraintName,
+                MOI.VariableName,
+                MOI.ObjectiveBound,
+            ],
+        ),
+        exclude = String[
+            # Expected test failures:
+            #   ArgumentError: The number of constraints must be greater than 0
+            "test_attribute_RawStatusString",
+            "test_attribute_SolveTimeSec",
+            "test_objective_ObjectiveFunction_blank",
+            "test_solve_TerminationStatus_DUAL_INFEASIBLE",
+            ##   Problem is a nonconvex QP
+            "test_basic_ScalarQuadraticFunction_EqualTo",
+            "test_basic_ScalarQuadraticFunction_GreaterThan",
+            "test_basic_ScalarQuadraticFunction_Interval",
+            "test_basic_VectorQuadraticFunction_",
+            "test_quadratic_SecondOrderCone_basic",
+            "test_quadratic_nonconvex_",
+            ##   MathOptInterface.jl issue #1431
+            "test_model_LowerBoundAlreadySet",
+            "test_model_UpperBoundAlreadySet",
+            # FIXME
+            #  Expression: ≈(MOI.get(model, MOI.ConstraintPrimal(), c2), 0, atol = atol, rtol = rtol)
+            #  Evaluated: 1.7999998823840366 ≈ 0 (atol=0.0001, rtol=0.0001)
+            "test_linear_FEASIBILITY_SENSE",
+            # FIXME
+            #  Error using pretransfo (line 149)
+            #  Size b mismatch
+            "test_conic_SecondOrderCone_negative_post_bound_ii",
+            "test_conic_SecondOrderCone_negative_post_bound_iii",
+            "test_conic_SecondOrderCone_no_initial_bound",
+            # TODO investigate
+            "test_conic_GeometricMeanCone_VectorAffineFunction_2",
+            "test_conic_GeometricMeanCone_VectorOfVariables_2",
+        ],
+    )
+    return
 end
 
-@testset "Continuous conic problems" begin
-    MOIT.contconictest(BRIDGED, CONFIG, [
-        # Unsupported cones
-        "pow", "dualpow", "rootdets", "exp", "dualexp", "logdet", "normspec", "normnuc", "relentr",
-        # TODO investigate
-        "geomean2v", "geomean2f"
-    ])
-end
+end  # module
+
+TestSeDuMi.runtests()
