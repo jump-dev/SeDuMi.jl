7ff52478cdb638c5cf6ea7ddf50349a6b727f36b
Benoît Legat <benoit.legat@gmail.com>
2022-03-01 19:45:42 -0500

    Update to MOI v1

--- test/MOI_wrapper.jl
+++ test/MOI_wrapper.jl
@@ -22,7 +22,17 @@ function test_solver_name()
 end
 
 function test_runtests()
-    model = MOI.instantiate(SeDuMi.Optimizer, with_bridge_type=Float64)
+    model = MOI.Utilities.CachingOptimizer(
+        MOI.Utilities.UniversalFallback(MOI.Utilities.Model{Float64}()),
+        MOI.instantiate(SeDuMi.Optimizer; with_bridge_type = Float64),
+    )
+    @test model.optimizer.model.model_cache isa
+          MOI.Utilities.UniversalFallback{SeDuMi.OptimizerCache}
+    # `Variable.ZerosBridge` makes dual needed by some tests fail.
+    MOI.Bridges.remove_bridge(
+        model.optimizer,
+        MathOptInterface.Bridges.Variable.ZerosBridge{Float64},
+    )
     MOI.set(model, MOI.Silent(), true)
     MOI.Test.runtests(
         model,
@@ -30,12 +40,10 @@ function test_runtests()
             rtol = 1e-4,
             atol = 1e-4,
             exclude = Any[
-                MOI.ConstraintFunction, # The bridge does not implement this
                 MOI.ConstraintBasisStatus,
                 MOI.VariableBasisStatus,
-                MOI.ConstraintName,
-                MOI.VariableName,
                 MOI.ObjectiveBound,
+                MOI.SolverVersion,
             ],
         ),
         exclude = String[
@@ -45,29 +53,27 @@ function test_runtests()
             "test_attribute_SolveTimeSec",
             "test_objective_ObjectiveFunction_blank",
             "test_solve_TerminationStatus_DUAL_INFEASIBLE",
-            ##   Problem is a nonconvex QP
-            "test_basic_ScalarQuadraticFunction_EqualTo",
-            "test_basic_ScalarQuadraticFunction_GreaterThan",
-            "test_basic_ScalarQuadraticFunction_Interval",
-            "test_basic_VectorQuadraticFunction_",
-            "test_quadratic_SecondOrderCone_basic",
-            "test_quadratic_nonconvex_",
-            ##   MathOptInterface.jl issue #1431
-            "test_model_LowerBoundAlreadySet",
-            "test_model_UpperBoundAlreadySet",
-            # FIXME
+            # TODO investigate
             #  Expression: ≈(MOI.get(model, MOI.ConstraintPrimal(), c2), 0, atol = atol, rtol = rtol)
             #  Evaluated: 1.7999998823840366 ≈ 0 (atol=0.0001, rtol=0.0001)
             "test_linear_FEASIBILITY_SENSE",
-            # FIXME
-            #  Error using pretransfo (line 149)
-            #  Size b mismatch
-            "test_conic_SecondOrderCone_negative_post_bound_ii",
-            "test_conic_SecondOrderCone_negative_post_bound_iii",
-            "test_conic_SecondOrderCone_no_initial_bound",
             # TODO investigate
+            # Incorrect solution
             "test_conic_GeometricMeanCone_VectorAffineFunction_2",
             "test_conic_GeometricMeanCone_VectorOfVariables_2",
+            # TODO investigate
+            # DimensionMismatch("dot product arguments have lengths 2 and 0")
+            "test_conic_SecondOrderCone_negative_post_bound_2",
+            "test_conic_SecondOrderCone_negative_post_bound_3",
+            "test_conic_SecondOrderCone_no_initial_bound",
+            # FIXME The objective has wrong sign
+            # See https://github.com/jump-dev/MathOptInterface.jl/issues/1759
+            "test_unbounded_MAX_SENSE",
+            "test_unbounded_MAX_SENSE_offset",
+            # FIXME The objective is wrong
+            # See https://github.com/jump-dev/MathOptInterface.jl/issues/1759
+            "test_unbounded_MIN_SENSE",
+            "test_unbounded_MIN_SENSE_offset",
         ],
     )
     return
